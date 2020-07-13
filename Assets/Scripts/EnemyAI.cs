db24b22dd8a35543e39676273ba3baf23e67fa9a
Dwyriel <Dwyriel333@hotmail.com>
2020-07-13 14:24:38 -0300

    Initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f, endChase = 15f, attackRange = 2.5f;
+    NavMeshAgent agent;
+    float distanceToTarget = Mathf.Infinity;
+    bool chasing = false, isProvoked = false;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = attackRange;
+    }
+    private void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, this.transform.position);
+        Chase();
+        Provoked();
+        PlayerIsVisible();
+    }
+    private void Provoked()
+    {
+        //! if attack 
+        //! isProvoked = true;
+        //? start Coroutine to set isProvoked back to false?
+    }
+
+    private void PlayerIsVisible()
+    {
+        if (distanceToTarget < chaseRange)
+        {
+            chasing = true;
+        }
+        else if (distanceToTarget > endChase)
+        {
+            chasing = false;
+        }
+    }
+
+    private void Chase()
+    {
+        if (chasing || isProvoked)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+            if (distanceToTarget < attackRange)
+            {
+                print("Attacking");
+            }
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, chaseRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(this.transform.position, endChase);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(this.transform.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float range = 150;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        RaycastHit rHit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out rHit, range))
+        {
+            print("Hit something: " + rHit.transform.name);
+        }
+    }
+}
